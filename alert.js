f67ab47bac4fc7122e03a41985d2825c70244a54
Kevin Stratvert <ampaein@gmail.com>
2026-01-29 13:22:11 +0100

    Interaction: alert, prompt and confirm

--- alert.js
+++ alert.js
@@ -1,5 +1,14 @@
-let admin;
-let name;
-name = 'John';
-admin = name;
-alert(admin);
+alert("Hello");
+
+let age = prompt('How old are you?',);
+
+alert(`You are ${age} years old!`);
+
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss );
+
+let userName = prompt("What is your name?", "Guest");
+
+alert(`Hello, ${userName}!`);
+
