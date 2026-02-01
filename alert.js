312a068de1f87473d5d21925570729780e86e13d
Kevin Stratvert <ampaein@gmail.com>
2026-02-01 10:05:19 +0100

    Comparisons

--- alert.js
+++ alert.js
@@ -1,14 +1,2 @@
-alert("Hello");
-
-let age = prompt('How old are you?',);
-
-alert(`You are ${age} years old!`);
-
-let isBoss = confirm("Are you the boss?");
-
-alert( isBoss );
-
-let userName = prompt("What is your name?", "Guest");
-
-alert(`Hello, ${userName}!`);
-
+let a = ("end" > "about");
+alert(a);
